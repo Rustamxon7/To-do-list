a66b47ef45657993c1bf1e0158d50a5f16d4f5ac
Rustam <69011963+Rustamxon7@users.noreply.github.com>
2021-08-25 17:39:54 +0500

    Upload files to main branch

---
+++ src/index.js
@@ -0,0 +1,39 @@
+/* eslint-disable no-unused-vars */
+import _, { add } from 'lodash';
+import './style.css';
+
+const list = document.getElementById('list');
+
+const doList = [
+  {
+    id: 10,
+    description: 'Go to the mall',
+    completed: false,
+  },
+  {
+    id: 1,
+    description: 'Clean your room',
+    completed: false,
+  },
+  {
+    description: 'Coding',
+    id: 2,
+    completed: false,
+  },
+];
+
+doList.sort((a, b) => a.id - b.id);
+
+const addToDo = (toDoList) => {
+  for (let id = 0; id < toDoList.length; id += 1) {
+    const item = `
+   <li>
+           <ion-icon id="${doList[id].id}" class="el-1" name="square-outline"></ion-icon>
+           <p class="el-2">${doList[id].description}</p>
+          <ion-icon id="${doList[id].completed}" class="el-3" name="ellipsis-vertical-outline"></ion-icon>
+        </li>`;
+    list.innerHTML += item;
+  }
+};
+
+addToDo(doList);
